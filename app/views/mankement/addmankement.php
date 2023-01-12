07a255efde3bb8ce7acda861b6a724d0388b4f31
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-01-12 20:14:57 +0100

    mankementen toevoegen

--- app/views/mankement/addmankement.php
+++ app/views/mankement/addmankement.php
@@ -1,20 +1,20 @@
-<?php require(APPROOT . '/views/includes/header.php');?>
-<h2><?= $data['title']; ?></h2>
+    <?php require(APPROOT . '/views/includes/header.php');?>
+    <h2><?= $data['title']; ?></h2>
 
-<h4><?=  $data['Kenteken']; ?></h4>
+    <h4><?=  $data['Kenteken']; ?></h4>
 
-<form action="<?= URLROOT ?>/mankement/addmankement" method="post">
-    <label for="Mankement">Mankement</label>
-    <br>
-    <input type="text " name="Mankement" id="Mankement">
-    <br>
+    <form action="<?= URLROOT ?>/mankement/addmankement" method="post">
+        <label for="Mankement">Mankement</label>
+        <br>
+        <input type="text " name="Mankement" id="Mankement">
+        <br>
 
-    <div class="MankementError"><?= $data['MankementError'];?></div>
-    <input type="hidden" name="AutoId" value="<?= $data['AutoId'];?>">
+        <div class="MankementError"><?= $data['MankementError'];?></div>
+        <input type="hidden" name="AutoId" value="<?= $data['AutoId'];?>">
 
-    <br>
-    
-    <input type="submit" value="Voer in">
-</form>
+        <br>
+        
+        <input type="submit" value="Voer in">
+    </form>
 
-<?php require(APPROOT . '/views/includes/footer.php');?>
+    <?php require(APPROOT . '/views/includes/footer.php');?>
